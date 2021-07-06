8413a304f00c0503e8ae20eb6365782346612729
gargiscode <gargi.pattanayak@gmail.com>
2021-07-06 10:48:09 -0700

    Add project files.

---
+++ ToDoNotes/AddNotesPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoNotes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddNotesPage : ContentPage
+    {
+        string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"notes.txt");
+        public AddNotesPage()
+        {
+            InitializeComponent();
+            if (File.Exists(fileName))
+            {
+                editor.Text = File.ReadAllText(fileName);
+            }
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(fileName, editor.Text);
+        }
+
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if(File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
---
+++ ToDoNotes/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoNotes.Models;
+using Xamarin.Forms;
+
+namespace ToDoNotes
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            //base.OnAppearing();
+
+            var notes = new List<Note>();
+            var files= Directory.EnumerateFiles(Environment.GetFolderPath
+                (Environment.SpecialFolder.LocalApplicationData), "*.notes.txt");
+
+            foreach (var filename in files)
+            {
+                var note = new Note
+                {
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename),
+                    FileName = filename
+                };
+                notes.Add(note);
+
+            }
+        }
+
+        private void NoteAddedClicked(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        private void OnNoteItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
